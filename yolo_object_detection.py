95677d7c417ed88c402174c246c5afa0303c1712
danivngopro <44786079+danivngopro@users.noreply.github.com>
2022-02-01 19:27:23 +0200

    Add files via upload

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -3,12 +3,97 @@ import numpy as np
 import glob
 import random
 
+from sqlalchemy import true
+
 
 # Load Yolo
 net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3_testing.cfg")
 
 # Name custom object
-classes = ["Koala"]
+classes = ["car"]
+
+# Images path
+images_path = glob.glob(r"D:\Pysource\Youtube\2020\105) Train Yolo google cloud\dataset\*.jpg")
+
+
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+import mss
+sct = mss.mss()
+
+# Insert here the path of your images
+random.shuffle(images_path)
+# loop through all the images
+while(True):
+    monitor = {'top': 0, 'left': 0, 'width': 2000, 'height': 1000}
+    img = np.array(sct.grab(monitor))
+    img = np.flip(img[:, :, :3], 2)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Loading image
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.3:
+                # Object detected
+                print(class_id)
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    print(indexes)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
+
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+import cv2
+import numpy as np
+import glob
+import random
+
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3_testing.cfg")
+
+# Name custom object
+classes = ["car"]
 
 # Images path
 images_path = glob.glob(r"D:\Pysource\Youtube\2020\105) Train Yolo google cloud\dataset\*.jpg")
